3a6a72b0704ba48687f06b5a85ef764ffaf57865
haekyu <gorb6593@gmail.com>
2024-08-03 00:04:32 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>진짜 div가 없네..</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
