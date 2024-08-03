55a93ec37470f6b1f6b741096efce6258a618918
haekyu <gorb6593@gmail.com>
2024-08-04 00:12:48 +0900

    이미지 작업 및 글꼴

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import Item from './components/item/Item';
+import {SafeAreaView, Image} from 'react-native';
+import MyText from './components/text/MyText';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Item name={'Table'} price={20} />
-      <Item name={'Chair'} price={2000} />
-      <Item name={'Desk'} price={200} />
+      <Image
+        source={require('./asset/images/test-image2.png')}
+        style={{width: 100, height: 100, backgroundColor: 'red'}}
+        resizeMode={'contain'}
+      />
+      <Image
+        source={{
+          uri: 'https://i.pinimg.com/474x/86/b3/bf/86b3bf1fe796de38564fb91b48a34e31.jpg',
+        }}
+        style={{width: 100, height: 100}}
+      />
     </SafeAreaView>
   );
 };
--- components/text/MyText.js
+++ components/text/MyText.js
@@ -3,8 +3,15 @@ import {Text} from 'react-native';
 import style from './style';
 
 const MyText = () => {
+  const handleTextClick = () => {
+    alert('click!!');
+  };
   return (
-    <Text style={[style.text, style.text1]}>Hello, React Native 프로젝트!</Text>
+    <Text
+      style={[style.text, style.text1, {color: 'green'}]}
+      onPress={() => handleTextClick()}>
+      Hello, React Native 프로젝트!
+    </Text>
   );
 };
 
